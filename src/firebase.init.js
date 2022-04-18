29eaa70e96817fbd6c38e14d9a375ee42b0400ad
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-04-18 20:30:24 +0600

    environment variable updated

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBlKMyC92p8hQGfh91q5N-VlnspfhBk0ZU",
-  authDomain: "jade-mills-real-estate-agent.firebaseapp.com",
-  projectId: "jade-mills-real-estate-agent",
-  storageBucket: "jade-mills-real-estate-agent.appspot.com",
-  messagingSenderId: "1003299476229",
-  appId: "1:1003299476229:web:ae3ff757d119a9ec2e9d6c"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
